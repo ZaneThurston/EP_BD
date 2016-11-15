3eaf0a2d8946d0d5a1b244b358d8d4c3115579d5
ZaneThruston <marcoshenrique0595@gmail.com>
2016-11-15 20:24:17 -0200

    Cadastro de aeronaves e de aeroportos quase funcional

--- src/PanelCadAeronaves.java
+++ src/PanelCadAeronaves.java
@@ -1,6 +1,21 @@
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.text.DefaultFormatterFactory;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.GroupLayout;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import java.awt.Dimension;
+import javax.swing.DefaultComboBoxModel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,6 +36,13 @@ public class PanelCadAeronaves extends JPanel {
                 jButtonSave,
                 jButtonClean;
     PanelCadAeronaves thisPanel = this;
+    private JLabel lblCadAero;
+    private JLabel lblNmeroDeSrie;
+    private JFormattedTextField NumSerie;
+    private JLabel lblMatrculaCaracteres;
+    private JTextField Matricula;
+    private JLabel lblModelo;
+    private JTextField Modelo;
     
     public PanelCadAeronaves(UserInterface window, BdConnector conectorBD, InitialPanel init) {
 
@@ -39,32 +61,115 @@ public class PanelCadAeronaves extends JPanel {
         jButtonSave.setText("Salvar");
 
         jButtonClean.setText("Limpar Campos");
+        jButtonClean.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				limpaCampos();
+			}
+		});
+        
+        lblCadAero = new JLabel("Cadastro de Aeronave na frota");
+        lblCadAero.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        
+        lblNmeroDeSrie = new JLabel("N\u00FAmero de s\u00E9rie:");
+        
+        NumSerie = new JFormattedTextField();
+        NumSerie.setFormatterFactory(new DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0"))));
+        lblMatrculaCaracteres = new JLabel("Matr\u00EDcula (5 caracteres, ex.: PTLSG):");
+        
+        Matricula = new JTextField();
+        Matricula.setColumns(10);
+        
+        lblModelo = new JLabel("Modelo");
+        
+        Modelo = new JTextField();
+        Modelo.setColumns(10);
+        
+        JLabel lblCat = new JLabel("Categoria:");
+        
+        JComboBox catBox = new JComboBox();
+        catBox.setModel(new DefaultComboBoxModel(new String[] {"Medium", "Heavy"}));
+        catBox.setMaximumSize(new Dimension(150, 20));
+        catBox.setFont(new Font("Dialog", Font.BOLD, 11));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButtonVoltar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 523, Short.MAX_VALUE)
-                        .addComponent(jButtonClean)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButtonSave)))
-                .addContainerGap())
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(layout.createSequentialGroup()
+        					.addComponent(jButtonVoltar)
+        					.addPreferredGap(ComponentPlacement.RELATED, 183, Short.MAX_VALUE)
+        					.addComponent(jButtonClean)
+        					.addGap(18)
+        					.addComponent(jButtonSave))
+        				.addComponent(lblCadAero)
+        				.addGroup(layout.createSequentialGroup()
+        					.addComponent(lblNmeroDeSrie)
+        					.addPreferredGap(ComponentPlacement.UNRELATED)
+        					.addComponent(NumSerie, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE))
+        				.addGroup(layout.createSequentialGroup()
+        					.addComponent(lblMatrculaCaracteres)
+        					.addPreferredGap(ComponentPlacement.RELATED)
+        					.addComponent(Matricula, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        				.addGroup(layout.createParallelGroup(Alignment.TRAILING, false)
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addComponent(lblCat)
+        						.addPreferredGap(ComponentPlacement.RELATED)
+        						.addComponent(catBox, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addComponent(lblModelo)
+        						.addPreferredGap(ComponentPlacement.RELATED)
+        						.addComponent(Modelo, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE))))
+        			.addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 407, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonVoltar)
-                    .addComponent(jButtonSave)
-                    .addComponent(jButtonClean))
-                .addContainerGap())
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(lblCadAero)
+        			.addGap(18)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblNmeroDeSrie)
+        				.addComponent(NumSerie, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblMatrculaCaracteres)
+        				.addComponent(Matricula, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblModelo)
+        				.addComponent(Modelo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblCat)
+        				.addComponent(catBox, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(jButtonVoltar)
+        				.addComponent(jButtonSave)
+        				.addComponent(jButtonClean))
+        			.addContainerGap())
         );
+        this.setLayout(layout);
+        jButtonSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//conectorBD.insereAeronave();
+				
+			}
+		});
     }
+    
+    public void limpaCampos() {
+    	NumSerie.setValue(null);
+       	Matricula.setText("");
+    	Modelo.setText("");
+    	
+    }
+    
+    
+    
 }
--- src/PanelCadAeroportos.java
+++ src/PanelCadAeroportos.java
@@ -1,6 +1,16 @@
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.JCheckBox;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,6 +28,15 @@ public class PanelCadAeroportos  extends JPanel {
                 jButtonSave,
                 jButtonClean;
     PanelCadAeroportos thisPanel = this;
+    private JLabel lblCadAero;
+    private JLabel lblNewLabel;
+    private JLabel lblAerNome;
+    private JLabel lblCidade;
+    private JTextField ICAO;
+    private JTextField nome;
+    private JTextField cidade;
+    private JCheckBox chkHub;
+    private JCheckBox chkOficina;
     
     public PanelCadAeroportos(UserInterface window, BdConnector conectorBD, InitialPanel init) {
 
@@ -34,34 +53,119 @@ public class PanelCadAeroportos  extends JPanel {
         });
 
         jButtonSave.setText("Salvar");
-
+        jButtonSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        
         jButtonClean.setText("Limpar Campos");
+        jButtonClean.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				limpaCampos();
+			}
+		});
+        
+        
+        lblCadAero = new JLabel("Cadastro de Aeroportos");
+        lblCadAero.setFont(new Font("Tahoma", Font.PLAIN, 13));
+        
+        lblNewLabel = new JLabel("C\u00F3digo ICAO (4 caracteres ex.: SBSP):");
+        
+        lblAerNome = new JLabel("Nome:");
+        
+        lblCidade = new JLabel("Cidade");
+        
+        ICAO = new JTextField();
+        ICAO.setColumns(10);
+        
+        nome = new JTextField();
+        nome.setColumns(10);
+        
+        cidade = new JTextField();
+        cidade.setColumns(10);
+        
+        chkOficina = new JCheckBox("Possui Oficina?");
+        
+        chkHub = new JCheckBox("Hub da companhia?");
+        
+        JCheckBox chkPark = new JCheckBox("Possui estacionamento de aeronaves?");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButtonVoltar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 523, Short.MAX_VALUE)
-                        .addComponent(jButtonClean)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButtonSave)))
-                .addContainerGap())
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(chkPark)
+        				.addComponent(chkHub)
+        				.addGroup(layout.createSequentialGroup()
+        					.addComponent(jButtonVoltar)
+        					.addPreferredGap(ComponentPlacement.RELATED, 366, Short.MAX_VALUE)
+        					.addComponent(jButtonClean)
+        					.addGap(18)
+        					.addComponent(jButtonSave))
+        				.addComponent(lblCadAero)
+        				.addGroup(layout.createSequentialGroup()
+        					.addComponent(lblCidade)
+        					.addPreferredGap(ComponentPlacement.UNRELATED)
+        					.addComponent(cidade, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE))
+        				.addGroup(layout.createParallelGroup(Alignment.TRAILING, false)
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addComponent(lblAerNome)
+        						.addPreferredGap(ComponentPlacement.UNRELATED)
+        						.addComponent(nome))
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addComponent(lblNewLabel)
+        						.addPreferredGap(ComponentPlacement.UNRELATED)
+        						.addComponent(ICAO, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+        				.addComponent(chkOficina))
+        			.addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 407, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonVoltar)
-                    .addComponent(jButtonSave)
-                    .addComponent(jButtonClean))
-                .addContainerGap())
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(lblCadAero)
+        			.addGap(18)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblNewLabel)
+        				.addComponent(ICAO, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblAerNome)
+        				.addComponent(nome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblCidade)
+        				.addComponent(cidade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(chkOficina)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(chkHub)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(chkPark)
+        			.addPreferredGap(ComponentPlacement.RELATED, 96, Short.MAX_VALUE)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(jButtonVoltar)
+        				.addComponent(jButtonSave)
+        				.addComponent(jButtonClean))
+        			.addContainerGap())
         );
+        this.setLayout(layout);
     }
+    
+    public void limpaCampos() {
+    	ICAO.setText("");
+    	nome.setText("");
+    	cidade.setText("");
+    	chkHub.setSelected(false);
+    	chkOficina.setSelected(false);
+    }
+    
 }
